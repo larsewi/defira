00004b1f2bb2704c74dd00abb391a518c8fe4b03
Lars Erik Wik <lars.erik.wik@northern.tech>
2023-07-07 19:39:45 +0200

    Added unit test
    Ticket: None
    Changelog: None
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub fn print_hello() {
     log::info!("Hello World!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
